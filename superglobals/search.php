a51ccefe303e0075468af1b75b38347e83e2ad4b
Nouster <djebali.mohamed88@gmail.com>
2023-02-28 19:58:33 +0100

    find username thanks to array_search function

--- superglobals/search.php
+++ superglobals/search.php
@@ -9,7 +9,38 @@ $name = $_GET['name'];
 
 $founded_name = array_search($name, array_column($users, 'name'));
 
-if ($founded_name === false) {
+if (!$founded_name) {
     exit('Pas de nom trouvé');
 }
 
+$founded_userName = $users[$founded_name];
+
+$founded_userName = null;
+
+foreach ($users as $user) {
+    if ($user['name'] === $founded_name) {
+      $founded_userName = $user['name'];
+    }
+  }
+
+if ($founded_userName === null) {
+    exit('Pas de nom trouvé');
+}
+
+if ($founded_userName !== $name) {
+    exit('Pas de nom trouvé');
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
