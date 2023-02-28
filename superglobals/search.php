38feb74b35f87da820de6a7fcb9178a705fd33e1
Nouster <djebali.mohamed88@gmail.com>
2023-02-28 21:26:35 +0100

    In progress

--- superglobals/search.php
+++ superglobals/search.php
@@ -2,34 +2,35 @@
 // Recherche utilisateur par nom
 // http://localhost/search.php?q=Mick
 require_once 'data/users.php';
-// Je récupére le nom dans les parametres de l'URL
+// Je récupère le nom dans les paramètres de l'URL
 $name = $_GET['name'];
 
-
-
 $founded_name = array_search($name, array_column($users, 'name'));
-
-if (!$founded_name) {
+if ($founded_name === false) {
     exit('Pas de nom trouvé');
 }
-
 $founded_userName = $users[$founded_name];
 
 $founded_userName = null;
 
 foreach ($users as $user) {
-    if ($user['name'] === $founded_name) {
-      $founded_userName = $user['name'];
+    if ($user['name'] === $name) {
+        $founded_userName = $user;
     }
-  }
+}
 
 if ($founded_userName === null) {
     exit('Pas de nom trouvé');
 }
 
-if ($founded_userName !== $name) {
-    exit('Pas de nom trouvé');
-}
+// if ($founded_user['name'] !== $name) {
+//     exit('Pas de nom trouvé');
+// }
+
+// if ($founded_userName === $founded_user['name']) {
+//     echo 'Le nom '.$founded_user['name']. 'est trouvée';
+// }
+
 ?>
 
 <!DOCTYPE html>
@@ -41,6 +42,6 @@ if ($founded_userName !== $name) {
     <title>Document</title>
 </head>
 <body>
-    
+   <h1><?php $founded_userName?></h1>
 </body>
 </html>
