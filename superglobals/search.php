faf45f2765840d12fede0875c71389657ada3e3f
Nouster <djebali.mohamed88@gmail.com>
2023-02-28 18:49:48 +0100

    Started search.php

--- superglobals/search.php
+++ superglobals/search.php
@@ -1,3 +1,15 @@
 <?php
 // Recherche utilisateur par nom
 // http://localhost/search.php?q=Mick
+require_once 'data/users.php';
+// Je récupére le nom dans les parametres de l'URL
+$name = $_GET['name'];
+
+
+
+$founded_name = array_search($name, array_column($users, 'name'));
+
+if ($founded_name === false) {
+    exit('Pas de nom trouvé');
+}
+
